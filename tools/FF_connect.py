604f64ba0780a3518b6894c7b096b1090a0ebeef
sichengl <111520990+sichengl@users.noreply.github.com>
2026-04-22 18:49:29 -0400

    Create FF_connect.py

---
+++ tools/FF_connect.py
@@ -0,0 +1,71 @@
+import h5py
+import numpy as np
+from pathlib import Path
+
+def concat_h5_in_directory(input_dir, output_dir, output_filename, dataset_name):
+    """
+    read all the HDF5 files under designated directory, and concatenate them with 
+    designated axis, defulted to 0
+
+    parameters:
+        input_dir (str): the directory that stores all the .h5 files
+        output_dir (str): diectory of the merged file (name not included)
+        output_filename (str): name of the merged file (eg. "N60G45MOM2p5_complete_.h5")
+        dataset_name (str): name of the dataset within the final merged file
+    """
+    input_path = Path(input_dir)
+    
+    file_list = sorted(
+        input_path.glob("*.h5"),
+        key=lambda f: int(f.stem.split("cfg")[-1]) #sort files according to the number after str "cfg"
+    )
+    
+    if not file_list:
+        print(f"No .h5 file found under path {input_dir} ！")
+        return
+
+    print(f" Found {len(file_list)} .h5 files. Now merging them...")
+
+    all_data = []
+    
+    #loop over files found
+    for fname in file_list:
+        try:
+            with h5py.File(fname, "r") as f:
+                if dataset_name in f:
+                    dset = f[dataset_name]
+                    data = dset[:]
+                    data = np.mean(data,axis=(-1))
+                    all_data.append(data)
+                    print(f" File found with name: {fname.name}")
+                else:
+                    print(f"Warining: {fname.name} doesn't have dataset '{dataset_name}'")
+        except Exception as e:
+            print(f"error when reading {fname.name} : {e}")
+
+    if not all_data:
+        print("No data extracted")
+        return
+
+    #merging data
+    complete_data = np.concatenate(all_data, axis=0)
+    print(f"shape of merged file: {complete_data.shape}")
+
+    output_path = Path(output_dir) / output_filename
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+
+    print(f"writing merged data to : {output_path}")
+    with h5py.File(output_path, "w") as f:
+        f.create_dataset(dataset_name, data=complete_data)
+        
+    print("Done")
+
+
+
+if __name__ == "__main__":
+    concat_h5_in_directory(
+        input_dir=f"/lustre/orion/lgt132/scratch/sicheng/gluon_gpd_benchmark/Fmunu/incomplete_corr",
+        output_dir=f"/lustre/orion/lgt132/scratch/sicheng/gluon_gpd_benchmark/complete_corr/",
+        output_filename=f"FF_corr_complete",
+        dataset_name=f"corr"
+    )
